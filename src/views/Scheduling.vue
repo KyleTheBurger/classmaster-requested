3715ae939e2b6f558f5dcc38b5cda44ba8cd898d
Age Reapor <diatum19@gmail.com>
2024-12-11 10:21:51 +0800

    Forecasting view

--- src/views/Scheduling.vue
+++ src/views/Scheduling.vue
@@ -1,15 +1,9 @@
 <template>
-    <div class="container">
-      Scheduling
-    </div>
-  </template>
-  
-  <script>
-  export default {
-  
-  }
-  </script>
-  
-  <style>
-  
-  </style>
+	<div class="container">Scheduling</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
